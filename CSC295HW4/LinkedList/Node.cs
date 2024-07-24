097aecb5d18c8c3ec41435e0dd3ec156e47b4905
pchai <prempreychhay@gmail.com>
2024-07-23 19:55:38 -0700

    Add project files.

---
+++ CSC295HW4/LinkedList/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC295HW4.LinkedList
+{
+    public class Node
+    {
+        // These two are the important part
+        // Can store int or string
+        public int Data { get; set; }
+        // Node? Next is going to point to the next bucket and so on until Null
+        public Node? Next { get; set; }
+
+        public void DisplayNode()
+        {
+            Console.WriteLine(Data);
+        }
+    }
+}
---
+++ CSC295HW4/Program.cs
@@ -0,0 +1,30 @@
+﻿using CSC295HW4.LinkedList;
+
+namespace CSC295HW4
+{
+    internal class Program
+    {
+        // Linked List
+        static void Main(string[] args)
+        {
+            Node nodeA = new Node();
+            nodeA.Data = 165;
+
+            Node nodeB = new Node();
+            nodeB.Data = 435;
+
+            Node nodeC = new Node();
+            nodeC.Data = 196;
+
+            Node nodeD = new Node();
+            nodeD.Data = 951;
+
+            // Make the arrow point to the next node
+            // A linked list is an objectified array
+            nodeA.Next = nodeB;
+            nodeB.Next = nodeC;
+            nodeC.Next = nodeD;
+        }
+    }
+
+}

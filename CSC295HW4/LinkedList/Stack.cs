62dc19b8066f3cf2971834d71f324d69b1662c46
pchai <prempreychhay@gmail.com>
2024-08-01 15:58:35 -0700

    Stack CSC295

---
+++ CSC295HW4/LinkedList/Stack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC295HW4.LinkedList
+{
+    public class MyStack
+    {
+        private LinkedList _stack = new LinkedList();
+
+        public LinkedList Stack { get { return _stack; } }
+
+        public void Push(int value)
+        {
+            _stack.InsertFirst(value);
+        }
+
+        public Node Pop()
+        {
+            return _stack.DeleteFirst();
+        }
+    }
+}
